b381a85320e3bfcad79223e832de736e89a654b1
Elijah Ahianyo <elijahahianyo@gmail.com>
2021-03-14 22:21:41 +0000

    reworked deployment

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,6 @@ scp  -o StrictHostKeyChecking=no -r ./env ./docker-compose.staging.yml ubuntu@$E
 #  #  ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
 #  #  chmod +x ./deploy.sh
 #scp  -o StrictHostKeyChecking=no -r  ./docker-compose.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/TradeProject/TradeEngine
-
 ssh -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ubuntu/TradeProject/TradeEngine
   export $(cat ./.env | xargs)
