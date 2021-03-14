c6f7556f823a763c610c5844158a3777295193a5
Elijah Ahianyo <elijahahianyo@gmail.com>
2021-03-14 22:03:36 +0000

    reworked deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 ssh -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP_ADDRESS  mkdir /home/ubuntu/app
-
+scp  -o StrictHostKeyChecking=no -r ./env ./docker-compose.staging.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/TradeProject/TradeEngine
 
 #mkdir -p ~/.ssh
 #echo "private key ---------"
@@ -13,5 +13,13 @@ ssh -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP_ADDRESS  mkdir /home/ubunt
 #ssh-add ~/.ssh/id_rsa
 #  #  ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
 #  #  chmod +x ./deploy.sh
-#scp  -o StrictHostKeyChecking=no -r  ./docker-compose.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/
+#scp  -o StrictHostKeyChecking=no -r  ./docker-compose.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/TradeProject/TradeEngine
+
+ssh -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
+  cd /home/ubuntu/TradeProject/TradeEngine
+  export $(cat ./.env | xargs)
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker pull elijahahianyo/trade-project:trade-engine-prod
+  docker-compose -f docker-compose.staging.yml  up -d
+ENDSSH
 
