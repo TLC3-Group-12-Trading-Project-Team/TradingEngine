84d76b7292616eda5d7702fb21f382a6a6d7591f
Elijah Ahianyo <elijahahianyo@gmail.com>
2021-03-14 16:54:11 +0000

    refactored deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.ssh
+echo "$PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa
+chmod 700 ~/.ssh/id_rsa
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+  #  ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
+  #  chmod +x ./deploy.sh
+scp  -o StrictHostKeyChecking=no -r  ./docker-compose.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/
