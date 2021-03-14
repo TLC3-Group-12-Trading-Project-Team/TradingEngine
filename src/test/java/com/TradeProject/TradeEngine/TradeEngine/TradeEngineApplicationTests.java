f1a507b8e5c2dda62e7abf7bd39f123373d57e12
Elijah Ahianyo <elijahahianyo@gmail.com>
2021-03-14 00:00:22 +0000

    initial

---
+++ src/test/java/com/TradeProject/TradeEngine/TradeEngine/TradeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradeProject.TradeEngine.TradeEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
