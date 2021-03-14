146bd82ac4c58684bf0c9eb2ac98b41097942087
Elijah Ahianyo <elijahahianyo@gmail.com>
2021-03-14 22:49:38 +0000

    reworked deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #ssh -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP_ADDRESS  mkdir /home/ubuntu/app
-scp  -o StrictHostKeyChecking=no -r ./env ./docker-compose.staging.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/TradeProject/TradeEngine
+scp  -o StrictHostKeyChecking=no -r ./.env ./docker-compose.staging.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/TradeProject/TradeEngine
 
 #mkdir -p ~/.ssh
 #echo "private key ---------"
