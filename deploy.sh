f79921fab77650683a4b69a8963bdbc1aa5a3284
Elijah Ahianyo <elijahahianyo@gmail.com>
2021-03-15 13:02:21 +0000

    added CI/CD and docker

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
-#ssh -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP_ADDRESS  mkdir /home/ubuntu/app
 scp  -o StrictHostKeyChecking=no -r ./.env ./docker-compose.staging.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/TradeProject/TradeEngine
-#mkdir -p ~/.ssh
-#echo "private key ---------"
-#echo "$SSH_KEY"
-#echo "$SSH_KEY" | tr -d '\r' > ~/.ssh/id_rsa
-#cat ~/.ssh/id_rsa
-#chmod 600 ~/.ssh/id_rsa
-#eval "$(ssh-agent -s)"
-#ssh-add ~/.ssh/id_rsa
-#  #  ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
-#  #  chmod +x ./deploy.sh
-#scp  -o StrictHostKeyChecking=no -r  ./docker-compose.yml ubuntu@$EC2_PUBLIC_IP_ADDRESS:/home/ubuntu/TradeProject/TradeEngine
 ssh -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ubuntu/TradeProject/TradeEngine
   export $(cat ./.env | xargs)
